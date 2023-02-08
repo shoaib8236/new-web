e7e8029aa843dc5df9c0723ef15181978ef84f7d
shoaib8236 <shoaib.raza.dev@gmail.com>
2023-02-09 02:20:50 +0500

    ui fixes

--- js/index.js
+++ js/index.js
@@ -13,13 +13,35 @@ let dropdown = async (x, action) => {
   }
 };
 
+let onOpenMobileMenu = (type) => {
+  let elem = document.getElementById("mobile_nav");
 
-document.addEventListener("DOMContentLoaded", function(){
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 50) {
-          document.getElementById('nav').classList.add('animated_nav');
-        } else {
-          document.getElementById('nav').classList.remove('animated_nav');
-        } 
-    });
-  }); 
+  if (elem.classList.contains("open_nav")) {
+    elem.classList.remove("open_nav");
+  } else {
+    elem.classList.add("open_nav");
+  }
+};
+
+let openMobileDropDown = (elem) => {
+  
+
+  if(elem.classList.contains("open_mobile_dropdown")) {
+    elem.classList.remove("open_mobile_dropdown");
+  }else {
+    elem.classList.add("open_mobile_dropdown");
+
+  }
+
+};
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      document.getElementById("nav").classList.add("animated_nav");
+    } else {
+      document.getElementById("nav").classList.remove("animated_nav");
+    }
+  });
+});
